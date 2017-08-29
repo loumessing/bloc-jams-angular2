c235390bbc0af18eef819b23cdb15535ba8fd369
loumessing <loumessing@gmail.com>
2017-08-29 17:28:07 -0600

    assignment-6: complete

---
+++ app/scripts/services/Fixtures.js
@@ -0,0 +1,78 @@
+(function(){
+  function Fixtures() {
+    var Fixtures = {};
+
+    var albumPicasso = {
+        title: 'The Colors',
+        artist: 'Pablo Picasso',
+        label: 'Cubism',
+        year: '1881',
+        albumArtUrl: 'assets/images/album_covers/01.png',
+        songs: [
+          //add actual time in seconds instead of strings
+            { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/bloc_jams_music/blue' },
+            { title: 'Green', duration: 103.96, audioUrl: 'assets/music/bloc_jams_music/green' },
+            { title: 'Red', duration: 268.45, audioUrl: 'assets/music/bloc_jams_music/red' },
+            { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/bloc_jams_music/pink'},
+            { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/bloc_jams_music/magenta'}
+        ]
+
+    };
+    var albumMarconi = {
+        title: 'The Telephone',
+        artist: 'Guglielmo Marconi',
+        label: 'EM',
+        year: '1909',
+        albumArtUrl: 'assets/images/album_covers/20.png',
+        songs: [
+            { title: 'Hello, Operator?', duration: '1:01' },
+            { title: 'Ring, ring, ring', duration: '5:01' },
+            { title: 'Fits in your pocket', duration: '3:21'},
+            { title: 'Can you hear me now?', duration: '3:14' },
+            { title: 'Wrong phone number', duration: '2:15'}
+        ]
+
+    };
+
+    var albumTheBooks = {
+      title: 'The Lemon of Pink',
+      artist: 'The Books',
+      label: 'Temporary Residence',
+      year: '2016',
+      albumArtUrl: 'assets/images/album_covers/14.png',
+      songs:[
+        {title: 'The Lemon of Pink', duration: 95, audioUrl: 'assets/music/bloc_jams_music/TheLemonofPinkI'},
+        {title: 'The Lemon of Pink II', duration: 95.11, audioUrl: 'assets/music/bloc_jams_music/TheLemonofPinkII'},
+        {title: 'Tokyo', duration: 223.21, audioUrl: 'assets/music/bloc_jams_music/Tokyo'},
+        {title: 'Bonanza', duration: 53, audioUrl: 'assets/music/bloc_jams_music/Bonanza'},
+        {title: 'S Is For EverySing', duration: 212.15, audioUrl: 'assets/music/bloc_jams_music/SisForEvrysing'},
+        {title: 'Explanation Mark', duration: 20, audioUrl: 'assets/music/bloc_jams_music/ExplanationMark'},
+        {title: 'There Is No There There', duration: 317.21, audioUrl: 'assets/music/bloc_jams_music/ThereisNoThere'},
+        {title: 'Take Time', duration: 317.12, audioUrl: 'assets/music/bloc_jams_music/TakeTime'},
+        {title: 'Dont Even Sing About It', duration: 250, audioUrl: 'assets/music/bloc_jams_music/DontEvenSingAboutIt'},
+        {title: 'The Future, Wouldnt That Be Nice', duration: 196, audioUrl: 'assets/music/bloc_jams_music/TheFutureWouldntThatBeNice'},
+        {title: 'A True Story of a Story of True Love', duration:265, audioUrl: 'assets/music/bloc_jams_music/ATrueStoryofaStoryofTrueLove'},
+        {title: 'The Right Aint Right', duration: 168, audioUrl: 'assets/music/bloc_jams_music/ThatRightAintShit'},
+        {title: 'P.S', duration: 56, audioUrl: 'assets/music/bloc_jams_music/PS'}
+      ]
+
+
+    };
+
+    Fixtures.getAlbum = function() {
+      return albumPicasso;
+    };
+    Fixtures.getCollection = function(numberOfAlbums){
+      var albums  = [];
+      for (var i = 0; i < numberOfAlbums; i++){
+        albums.push(this.getAlbum());
+      }
+      return albums;
+    };
+    return Fixtures;
+  }
+
+  angular
+    .module('blocJams')
+    .factory('Fixtures', Fixtures);
+})();
